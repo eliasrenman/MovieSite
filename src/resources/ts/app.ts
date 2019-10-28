cff7f77afd6b8ca67865ac6d66b5c60b5c2b195e
abborren <elias.renman00@gmail.com>
2019-10-28 13:22:50 +0100

    changed vue setyp

--- src/resources/ts/app.ts
+++ src/resources/ts/app.ts
@@ -1,21 +1,20 @@
 import Vue from 'vue';
 import navbar from './components/navbar/navbar.vue';
 import tiles from './components/featured/tiles.vue';
+import details from './components/details/details.vue';
+
 
 Vue.config.productionTip = false;
 /** navbar components */
-Vue.component('navbar', navbar);
+Vue.component('vue-nav', navbar);
 Vue.component('vue-tiles', tiles);
+Vue.component('vue-details', details);
 
 /**Feaured tiles components */
 
 // url for getting images.
 // https://image.tmdb.org/t/p/w500/imgId
 
-new Vue({})
-    .$mount('#vue-nav');
-
-
-new Vue({})
-    .$mount('#vue-tiles');
-
+new Vue({
+    el: '#app',
+})
