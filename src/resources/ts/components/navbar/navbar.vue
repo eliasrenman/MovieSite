1a77736f9f83e9578bc1163def833fad746ed51c
Elliott Sparrman <elliott.sparrman@learnet.se>
2019-10-30 15:34:00 +0100

    remade details layout

--- src/resources/ts/components/navbar/navbar.vue
+++ src/resources/ts/components/navbar/navbar.vue
@@ -7,6 +7,7 @@
             <vueMenu ref="vue_menu"></vueMenu>
         </div>
         <search></search>
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/powered-by-rectangle-blue-61ce76f69ce1e4f68a6031d975df16cc184d5f04fa7f9f58ae6412646f2481c1.svg">
     </div>
 </template>
 
@@ -40,6 +41,11 @@ export default {
 
         form {
             margin-left: auto;
+        }
+
+        & > img {
+            height: $menu-height;
+            margin-left: 15px;
         }
 
     }
