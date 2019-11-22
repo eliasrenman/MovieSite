8a857b69df6b3aff3a0ca9c5ecc1b2a3c13d5541
Abborren <elias.renman00@gmail.com>
2019-11-22 10:13:51 +0100

    done stuff

--- src/resources/ts/components/navbar/scroll-top.vue
+++ src/resources/ts/components/navbar/scroll-top.vue
@@ -1,11 +1,17 @@
 <template>
-  <go-top
+    <div class="container">
+        <div @click="scrollTop()">
+
+            Hello world
+        </div>
+    </div>
+  <!-- <go-top
     :size=60
     :bottom-gap=50
-    :right=170
+    :right=120
     :bg-color="'#6699cc'"
     :box-shadow="'5px 5px 10px rgba(0,0,0,0.5)'"
-  ></go-top>
+  ></go-top> -->
 </template>
 
 <script>
@@ -14,6 +20,24 @@ import GoTop from '@inotom/vue-go-top';
 export default {
   components: {
     GoTop
-  }
+  },
+  methods: {
+    scrollTop() {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+
+        // this.moveToDown = !this.moveToDown
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+    .container{ 
+        position: fixed;
+        z-index: 1000;
+    }
+</style>
