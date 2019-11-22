734825dc3eaf4ed477614c68b90eb1f873612ea0
Abborren <elias.renman00@gmail.com>
2019-11-22 11:24:37 +0100

    added backend logic for trending

--- src/controllers/api/v1/ApiTrendingController.ts
+++ src/controllers/api/v1/ApiTrendingController.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from "express-serve-static-core";
 import { movieDbGet } from '../../../shared/ApiGet';
-class ApiController {
+class ApiTrendingController {
     
     /**
      * Ignored.
@@ -9,13 +9,71 @@ class ApiController {
     }
 
     /**
-     * Trending controller logic
      * 
      * @param req Request
      * @param res Response
      * @swagger
      *
      * /api/v1/trending:
+     *   get:
+     *     description: Sends request to get 20 trending movies and series.
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: page
+     *         description: Specified page to get trending movies from.
+     *         in: query
+     *         required: false
+     *         type: int
+     *     tags:
+     *         - trending
+     *     responses:
+     *       200:
+     *         description: Successfully returns a json containing 20 movies and series.
+     */
+    public async index(req: Request, res: Response) {
+        let page = "";
+        if (!isNaN(req.query.page))
+            page = "page=" + req.query.page;
+        res.send(await movieDbGet('trending/all/week?'+ page));
+    }
+    
+    /**
+     * 
+     * @param req Request
+     * @param res Response
+     * @swagger
+     *
+     * /api/v1/trending/tv:
+     *   get:
+     *     description: Sends request to get 20 trending series.
+     *     produces:
+     *       - application/json
+     *     parameters:
+     *       - name: page
+     *         description: Specified page to get trending series from.
+     *         in: query
+     *         required: false
+     *         type: int
+     *     tags:
+     *         - trending
+     *     responses:
+     *       200:
+     *         description: Successfully returns a json containing 20 series.
+     */
+    public async tv(req: Request, res: Response) {
+        let page = "";
+        if (!isNaN(req.query.page))
+            page = "page=" + req.query.page;
+        res.send(await movieDbGet('trending/tv/week?'+ page));
+    }
+    /**
+     * 
+     * @param req Request
+     * @param res Response
+     * @swagger
+     *
+     * /api/v1/trending/movie:
      *   get:
      *     description: Sends request to get 20 trending movies.
      *     produces:
@@ -24,20 +82,20 @@ class ApiController {
      *       - name: page
      *         description: Specified page to get trending movies from.
      *         in: query
-     *         required: true
+     *         required: false
      *         type: int
      *     tags:
-     *         - collection
+     *         - trending
      *     responses:
      *       200:
      *         description: Successfully returns a json containing 20 movies.
      */
-    public async trending(req: Request, res: Response) {
-            let page = "";
+    public async movie(req: Request, res: Response) {
+        let page = "";
         if (!isNaN(req.query.page))
             page = "page=" + req.query.page;
-            res.send(await movieDbGet('trending/all/week?'+ page));
+        res.send(await movieDbGet('trending/movie/week?'+ page));
     }
     
 }
-export default ApiController;
+export default ApiTrendingController;
