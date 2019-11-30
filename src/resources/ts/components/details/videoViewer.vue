5ed20d15f4e06989dc2d8566553f189fce1fcdf4
Abborren <elias.renman00@gmail.com>
2019-11-30 14:13:14 +0100

    fixed sizing of youtube video

--- src/resources/ts/components/details/videoViewer.vue
+++ src/resources/ts/components/details/videoViewer.vue
@@ -1,9 +1,12 @@
 <template>
-    <iframe width="560" height="315" :src="'https://www.youtube.com/embed/' + trailer" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-    </iframe>
+    <div class="container">
+
+        <iframe class="video"  :src="'https://www.youtube.com/embed/' + trailer" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+        </iframe>
+    </div>
 
 </template>
 
@@ -26,7 +29,18 @@ export default {
     }
 }
 </script>
-
-<style>
-
+<style lang="scss" scoped>
+    .container {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-bottom: 56.25%;
+    }
+    .video {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
 </style>
